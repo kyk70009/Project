e5eae6e1f33060f6b3763925a0bb3dd1528da352
kyk70009 <150650859+kyk70009@users.noreply.github.com>
2023-12-21 11:42:52 +0900

    Add files via upload

---
+++ todolist/todolist/EntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EntryViewController.swift
+//  Example
+//
+//  Created by t2023-m0036 on 12/21/23.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
